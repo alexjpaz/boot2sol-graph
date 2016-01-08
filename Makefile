49d70f8c64943159e49b60b560d48553f734d060
Alex Paz <alexjpaz@gmail.com>
2016-01-08 16:14:38 -0500

    check

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 default: boot2sol
-	echo "lol"
-
+	python check.py
 
 boot2sol:
 	git clone https://github.com/masneyb/boot2sol.git
--- check.py
+++ check.py
@@ -1,8 +1,49 @@
 import json
+import sys
+import os
+
+class Expando(object):
+      pass
+
+data = Expando()
+
+os.chdir('boot2sol')
+os.system('git clean -fd')
+os.system('git reset --hard origin/master')
+
+p = os.popen('git log --format=%H', "r")
+
+data.commits = []
+
+while 1:
+  line = p.readline()
+  if not line: break
+  commit = Expando()
+  commit.sha = line.strip()
+
+
+  os.system('git clean -fd')
+  os.system('git reset --hard ' + commit.sha)
+
+  commit.message = os.popen("git log --format=%B", "r").read()
+
+
+  try:
+
+    os.system('head -n $(($(wc -l < "boot.asm") - 2)) "boot.asm"')
+
+    os.system('make')
+
+    commit.size = os.path.getsize('boot.bin')
+
+  except Exception, e:
+    commit.error = str(e)
+
+  data.commits.append(commit.__dict__)
 
 
 
 
 
-if __name__ == "__main__":
-  main()
+with open('../data.json', 'w') as outfile:
+  json.dump(data.__dict__, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
